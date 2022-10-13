3c386ef568f87699b42d71b798579b1b372dc3d0
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-10-13 19:44:24 +0600

    login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,11 +13,21 @@
                 <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
-
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
+
+            <div class="mt-4">
+                <x-jet-label for="phone" value="{{ __('phone') }}" />
+                <x-jet-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="address" value="{{ __('adress') }}" />
+                <x-jet-input id="adress" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required />
+            </div>
+
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
