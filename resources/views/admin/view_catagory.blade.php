27e7aefb422e7e5e2c4aaaf27040bd0f5e417f69
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-10-17 20:55:55 +0600

    some change

--- resources/views/admin/view_catagory.blade.php
+++ resources/views/admin/view_catagory.blade.php
@@ -1,6 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+
+<style type="text/css">
+     .div_center{
+        text-align: center;
+        padding-top: 40px;
+     }
+     .h1_font{
+        font-size: 30px;
+
+        padding-bottom: 20px;
+     }
+     .input_clolor{
+        color: rgb(241, 10, 10);
+        text color: black;
+
+     }
+
+     </style>
+
+
     <!-- Required meta tags -->
     @include('admin.css')
   </head>
@@ -13,7 +38,19 @@
 
 
     <!-- body -->
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="div_center">
+                <h6 class="h1_font">Add Category</h6>
+                <form>
+                    <input class="input_color"type="text" name="catagory" placeholder="Enter Category Name">
 
+                    <input type="submit"class="btn btn-primary"name="submit" value="Add Category">
+                </form>
+
+        </div>
+        </div>
+    </div>
 
 
     <!-- plugins:js -->
